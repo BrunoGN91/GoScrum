4325008bf4218a9baee4a7b3cf02c9e88841cacb
BrunoGN91 <bruno.gazzera.nicolas@gmail.com>
2022-06-28 16:19:39 -0300

    Proyect upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <form action="">
+        <h1>Iniciar sesión</h1>
+        <div>
+          <label htmlFor="">Email</label>
+          <input type="email" name="email"/>
+        </div>
+        <div>
+          <label htmlFor="">Contraseña</label>
+          <input type="password" name="password"/>
+        </div>
+        <button>Enviar</button>
+      </form>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
