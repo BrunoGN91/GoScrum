29d77cdda19554263071f2f6cd4f41f6b6b4ed6f
BrunoGN91 <bruno.gazzera.nicolas@gmail.com>
2022-07-02 13:29:18 -0300

    Added validations with yup

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './TaskForm.styles.css'
 import {useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const TaskForm = () => {
+    const requiredField = "* El campo es olbigatorio"
+    const validationSchema = () => 
+    
+    Yup.object().shape({
+      title: Yup.string().min(5, "* El título debe tener al menos 5 caracteres").required(requiredField),
+      status: Yup.string().required(requiredField),
+      priority: Yup.string().required(requiredField)
 
+    })
+    
 
     const initialValues = {
       title: '',
@@ -16,9 +26,9 @@ const TaskForm = () => {
         alert('ok')
       }
   
-      const formik = useFormik({initialValues, onSubmit});
+      const formik = useFormik({initialValues, onSubmit, validationSchema });
   
-      const { handleSubmit, handleChange} = formik
+      const { handleSubmit, handleChange, errors, touched, handleBlur} = formik
   
 
 
@@ -35,28 +45,40 @@ const TaskForm = () => {
                     name='title'
                     type="text" 
                     onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={errors.title ? "errors" : ''}
                     />
+                    {errors.title && touched.title && <span>{errors.title}</span>}
                 </div>
+                
                 <div>
                 <select
                 onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.status ? "errors" : ''}
                 name="status">
                         <option value="" >Seleccionar opción</option>
                         <option value="new">Nueva</option>
                         <option value="inProcess">En proceso</option>
                         <option value="finished">Terminada</option>
-                </select>
+                </select>   
+                {errors.status && touched.status && <span>{errors.status}</span>}          
                 </div>
+                
                 <div>
                 <select
                 onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.priority ? "errors" : ''}
                 name="priority">
                         <option value="" >Seleccionar opción</option>
                         <option value="low">Baja</option>
                         <option value="medium">Media</option>
                         <option value="high">Alta</option>
-                </select>
+                </select>   
+                {errors.priority && touched.priority && <span>{errors.priority}</span>}          
                 </div>
+                
             </div>
             <div>
                 <textarea 
