64d244784198f90f73371a815268cb830b3c940f
BrunoGN91 <bruno.gazzera.nicolas@gmail.com>
2022-07-11 16:32:00 -0300

    Updates

---
+++ static/js/70.afe73dcc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoscrum=self.webpackChunkgoscrum||[]).push([[70],{7070:function(r,c,e){e.r(c);e(2791);var n=e(184);c.default=function(){return(0,n.jsx)("div",{className:"container",children:"Error404"})}}}]);
+//# sourceMappingURL=70.afe73dcc.chunk.js.map
