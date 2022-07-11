0b2402bf2cf8cb590e82b1871280110f94f97298
BrunoGN91 <bruno.gazzera.nicolas@gmail.com>
2022-07-11 16:39:42 -0300

    Added HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import {Provider} from 'react-redux'
 import { store } from './store/store';
 
@@ -10,10 +10,10 @@ import { store } from './store/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='GoScrum/'>
+    <HashRouter >
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
